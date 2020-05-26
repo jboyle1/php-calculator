621f9ee0b203b6f9f3e617b7c1984cf53080c1c3
Jed Boyle <jedboyle01@gmail.com>
2020-05-26 20:29:28 +0100

    Add static folder and inside create a CSS directory and a JS directory. Inside the CSS directory create a reset.css file and input reset configuration. Also create a main styles.css file.

---
+++ addition.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+        <h1><?="The sum of ${_GET['add_first']} and ${_GET['add_second']} is:"?></h1>
+        <h4><?=$_GET['add_first']+$_GET['add_second'];?></h4>
+        <a href="index.html">Reset</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,11 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>PHP Calculator</title>
+        <title>Document</title>
     </head>
     <body>
         <h3>Addition</h3>
-        <form action="addition.html" method="get">
+        <form method="get" action="addition.php">
             <label>First Number:</label>
             <input type="number" name="add_first"><br>
             <label>Second Number:</label>
@@ -16,13 +16,12 @@
             <button type="submit">Add</button><br>
         </form>
         <h3>Division</h3>
-        <form action="division.html" method="get">
+        <form method="get" action="division.php">
             <label>Numerator:</label>
             <input type="number" name="div_num"><br>
             <label>Denominator:</label>
             <input type="number" name="div_den"><br>
             <button type="submit">Divide</button><br>
         </form>
-        
     </body>
 </html>
